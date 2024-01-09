0eb07c122dd9ebcac366a93c5b15f053ac9cb4cc
rehmanjameel <abdulrehmancs17@gmail.com>
2024-01-09 23:42:46 +0500

    Initial commit 2

--- app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
+++ app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
@@ -2,7 +2,6 @@ package org.codebase.ktlinittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
