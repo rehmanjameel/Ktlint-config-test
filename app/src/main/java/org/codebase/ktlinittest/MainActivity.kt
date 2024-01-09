863a088cc1997969d48aa92fd1f01017167d07c7
rehmanjameel <abdulrehmancs17@gmail.com>
2024-01-10 00:11:20 +0500

    testing`1

--- app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
+++ app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
@@ -1,11 +1,13 @@
 package org.codebase.ktlinittest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("tag", "message")
     }
 }
