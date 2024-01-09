a76f99feedb5e7d73c3dd0e08a0f032acb3752cd
rehmanjameel <abdulrehmancs17@gmail.com>
2024-01-09 23:50:00 +0500

    Initial commit 4

--- app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
+++ app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
@@ -2,13 +2,9 @@ package org.codebase.ktlinittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
-
-
