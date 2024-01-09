27ab3588b1dbc88eb1b9d39e40ba39dd82283e13
rehmanjameel <abdulrehmancs17@gmail.com>
2024-01-10 01:31:37 +0500

    testing`6

--- app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
+++ app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
@@ -2,8 +2,6 @@ package org.codebase.ktlinittest
 
 import android.os.Bundle
 import android.util.Log
-
-
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
