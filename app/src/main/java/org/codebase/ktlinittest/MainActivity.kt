d92111dc5c92dbf6bec07d0ed36d6703511f96fb
rehmanjameel <abdulrehmancs17@gmail.com>
2024-01-10 04:11:30 +0500

    testing 8

--- app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
+++ app/src/main/java/org/codebase/ktlinittest/MainActivity.kt
@@ -1,10 +1,8 @@
 package org.codebase.ktlinittest
 
-import android.os.Bundle
-
-import android.util.Log
-
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
