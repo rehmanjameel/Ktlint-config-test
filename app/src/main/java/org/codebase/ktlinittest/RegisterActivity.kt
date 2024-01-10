0dc878fe7563361be5fa5ee7273b9594e33cc229
rehmanjameel <abdulrehmancs17@gmail.com>
2024-01-10 19:03:32 +0500

    simple test7676

--- app/src/main/java/org/codebase/ktlinittest/RegisterActivity.kt
+++ app/src/main/java/org/codebase/ktlinittest/RegisterActivity.kt
@@ -1,9 +1,9 @@
 package org.codebase.ktlinittest
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 
-import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
